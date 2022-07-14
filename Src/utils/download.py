4b20a93cc423c84333bb394c519f254b0316928c
CristianLazoQuispe <mecatronico.lazo@gmail.com>
2022-07-14 18:56:46 -0500

    Download Dataset notebooks

---
+++ Src/utils/download.py
@@ -0,0 +1,18 @@
+import os, requests
+
+def download_dataset(fname,url):
+    if not os.path.isfile(fname):
+        try:
+            r = requests.get(url)
+        except requests.ConnectionError:
+            print("!!! Failed to download data !!!")
+        else:
+            if r.status_code != requests.codes.ok:
+                print("!!! Failed to download data !!!")
+            else:
+                with open(fname, "wb") as fid:
+                    fid.write(r.content)
+                    return True
+        return False
+    else:
+        return True
